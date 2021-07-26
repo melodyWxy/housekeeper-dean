14e01bba64d84a443650fb0762da26e6be324eec
朽木白 <1547702880@qq.com>
2021-07-26 16:37:51 +0800

    feat：+日程增删改查

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const { Command } = require("commander");
 const program = new Command();
+const fs = require('fs');
+const path = require('path');
+const cache = require('./cache/index.json');
 class Main {
    start() {
     // 版本
@@ -15,22 +18,36 @@ class Main {
       .action(async (arg)=>{
           // todosth
           console.log('查询日程');
+          console.table(cache);
       })
 
     // 增加日程
     program.command("add")
         .description('增加日程')
         .action(async (pk)=>{
-             // todosth
-            console.log('增加日程');
+            console.info('增加日程');
+            cache.push({
+              "createTime": "2021-07-26 00:00:00",
+              "startTime": "2021-07-26 00:00:00",
+              "endTime": "2021-07-28 00:00:00",
+              "des": "这是一段描述"
+            })
+            this.writeFile(cache);
         })
 
     // 更新日程
-    program.command("update [id]")
+    program.command("update [id] [des]")
         .description( "更新日程")
-        .action(async (id)=>{
+        .action(async (id, des)=>{
             // todosth
-            console.log('更新日程', id);
+            console.log('更新日程', id, des);
+            const update_id = Number(id);
+            cache.map((item, index) => {
+              if (index === update_id) {
+                return item.des = des;
+              }
+            })
+            this.writeFile(cache);
         })
 
     // 删除日程
@@ -39,10 +56,21 @@ class Main {
         .action(async (id)=>{
             // todosth
             console.log('删除日程', id);
+            const update_id = Number(id);
+            cache.splice(update_id, 1);
+            this.writeFile(cache);
         })
 
     // 解析环境参数，不要删除
     program.parse(process.argv);
+  }
+  writeFile(file) {
+    try {
+      fs.writeFileSync(path.resolve(__dirname, './cache/index.json'), JSON.stringify(file, null, 4));
+    } catch (e) {
+      console.error('🎵日程添加失败，你个完犊子怂货！！！');
+      process.exit();
+    }
   }
 }
 
